26a520f31c6d0aee5d3a1c97204e2c51a4091ad2
Johannes Berg <johannes.berg@intel.com>
2023-06-04 12:11:28 +0300

    wifi: mac80211: stop warning after reconfig failures
    If we have a reconfig failure in the driver, then we need
    to shut down the network interface(s) at the network stack
    level through cfg80211, which can result in a lot of those
    "Failed check-sdata-in-driver check, ..." warnings, since
    interfaces are considered to not be in the driver when the
    reconfiguration fails, but we still need to go through all
    the shutdown flow.
    
    Avoid many of these warnings by storing the fact that the
    stack experienced a reconfiguration failure and not doing
    the warning in that case.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Gregory Greenman <gregory.greenman@intel.com>
    Link: https://lore.kernel.org/r/20230604120651.3750c4ae6e76.I9e80d6026f59263c008a1a68f6cd6891ca0b93b0@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    (cherry picked from commit c4fdb0818d38fbe1ea7021fb51346df70bbca284)

--- net/mac80211/ieee80211_i.h
+++ net/mac80211/ieee80211_i.h
@@ -1392,6 +1392,9 @@ struct ieee80211_local {
 	/* device is during a HW reconfig */
 	bool in_reconfig;
 
+	/* reconfiguration failed ... suppress some warnings etc. */
+	bool reconfig_failure;
+
 	/* wowlan is enabled -- don't reconfig on resume */
 	bool wowlan;
 
