7b20dad5fed385aea00cfa4168809ac0bacd3782
Benjamin Berg <benjamin.berg@intel.com>
2023-09-28 17:35:29 +0300

    wifi: mac80211: mesh: fix some kdoc warnings
    These were mostly missing or incorrectly tagged return values.
    
    Signed-off-by: Benjamin Berg <benjamin.berg@intel.com>
    Signed-off-by: Gregory Greenman <gregory.greenman@intel.com>
    Link: https://lore.kernel.org/r/20230928172905.33fea2968c62.I41d197b570370ab7cad1405518512fdd36e08717@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    (cherry picked from commit c00de1c49294cb83ecf11c2f9306df5fec5c16a0)

--- net/mac80211/mesh_plink.c
+++ net/mac80211/mesh_plink.c
@@ -152,6 +152,8 @@ out:
  * selected if any non-HT peers are present in our MBSS.  20MHz-protection mode
  * is selected if all peers in our 20/40MHz MBSS support HT and at least one
  * HT20 peer is present. Otherwise no-protection mode is selected.
+ *
+ * Returns: BSS_CHANGED_HT or 0 for no change
  */
 static u32 mesh_set_ht_prot_mode(struct ieee80211_sub_if_data *sdata)
 {
@@ -354,7 +356,7 @@ free:
  * Mesh paths with this peer as next hop should be flushed
  * by the caller outside of plink_lock.
  *
- * Returns beacon changed flag if the beacon content changed.
+ * Returns: beacon changed flag if the beacon content changed.
  *
  * Locking: the caller must hold sta->mesh->plink_lock
  */
@@ -382,6 +384,8 @@ static u32 __mesh_plink_deactivate(struct sta_info *sta)
  * @sta: mesh peer link to deactivate
  *
  * All mesh paths with this peer as next hop will be flushed
+ *
+ * Returns: beacon changed flag if the beacon content changed.
  */
 u32 mesh_plink_deactivate(struct sta_info *sta)
 {
